da09bd080680f152054d46031fef52af0a10086e
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-01-13 16:06:14 -0500

    10 done

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,6 +1,12 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def is_even(n):
+    if n % 2 == 0:
+        return True
+
+print(is_even(4))          
+
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
@@ -10,3 +16,7 @@ num = int(num)
 
 # YOUR CODE HERE
 
+if num % 2 == 0: 
+    print("even")
+else: 
+    print("odd")   

a1ddd239eb0f9f9051cb8f7af0fc443453ecaa83
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-01-13 23:00:32 -0500

    working on 13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+f = open('./src/foo.txt', 'r')
+
+for line in f: 
+    print(line, end='')
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
